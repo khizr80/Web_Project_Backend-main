1c5fc725d5342fd46a151071ecf6acd2596f94bc
khizr80 <khizr80@gmail.com>
2025-06-02 23:51:29 +0500

    mm

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-// import { app } from "./app.js";
+import { app } from "./app.js";
 import express from "express";
 
-// dotenv.config({
-//   path: ".env",
-// });
+dotenv.config({
+  path: ".env",
+});
 
-const app = express();
+// const app = express();
 app.listen(8000, () => {
   console.log("Server is running at port : 8000");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// connectDB().catch((error) => {
-//   console.error("MONGO DB connection FAILED !! ", error);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
 // });
+
+connectDB().catch((error) => {
+  console.error("MONGO DB connection FAILED !! ", error);
+});
